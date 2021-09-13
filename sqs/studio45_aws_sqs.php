8fd55dfeb72fe152f46376f85804c30c5f6823f3
vijaystudio45 <vijaya.logiclab@gmail.com>
2021-09-13 11:45:11 +0530

    aws_sqs

--- sqs/studio45_aws_sqs.php
+++ sqs/studio45_aws_sqs.php
@@ -1,63 +1,42 @@
 <?php
-require 'sqs/vendor/autoload.php'
-    use Aws\Exception\AwsException;
-    use Aws\Credentials\Credentials;
-    use Aws\Sqs\SqsClient; 
-    $credentials = new Credentials('Enter Your Access key Here', 'Enter Your Secret Access Key Here')
-        $sqsClient = new SqsClient([
-            'version' => '2012-11-05',
-        'credentials' => $credentials,
-        'region' => 'Enter Your Region Here',
-    ]);
+require 'sqs/vendor/autoload.php';
+use Aws\Sqs\SqsClient;
+$client = new SqsClient([
+    'region' => 'ap-south-1',
+    'version' => 'latest',
+    'credentials' => [
+        'key' => 'Enter Your Access key Here',
+        'secret' => 'Enter Your Secret Access Key Here',
+        ]
+]);
 
-    //Debug values, these would later be filled from database
-    $params = [
-        'DelaySeconds' => 10,
-        'MessageAttributes' => [
-            "SocialSecurityCode" => [
-                'DataType' => "String",
-                'StringValue' => "111111-1"
-            ],
-            "Name" => [
-                'DataType' => "String",
-                'StringValue' => "Teemu Testaaja"
-            ],
-            "PeriodStart" => [
-                'DataType' => "String",
-                'StringValue' => "1-1-2019"
-            ],
-            "PeriodEnd" => [
-                'DataType' => "String",
-                'StringValue' => "31-1-2019"
-            ],
-            "EventDate" => [
-                'DataType' => "String",
-                'StringValue' => "12-1-2019"
-            ],
-            "Code" => [
-                'DataType' => "Number",
-                'StringValue' => "12"
-            ],
-            "Amount" => [
-                'DataType' => "Number",
-                'StringValue' => "7.5"
-            ]
+$params = [
+    'DelaySeconds' => 0,
+    'MessageAttributes' => [
+        "Title" => [
+            'DataType' => "String",
+            'StringValue' => "The Hitchhiker's Guide to the Galaxy"
         ],
-        'MessageBody' => "Test message",
-        'QueueUrl' => 'sqs-url from the console'
-    ];
+        "Author" => [
+            'DataType' => "String",
+            'StringValue' => "Douglas Adams."
+        ],
+        "WeeksOn" => [
+            'DataType' => "Number",
+            'StringValue' => "6"
+        ]
+    ],
+    'MessageBody' => "Hello Tarsem How are you.",
+    'QueueUrl' => 'Enter Your Queueurl Here'
+];
 
-    try {
-        print("Sending message");
-        $result = $sqsClient->sendMessage($params);
-        print("Message send");
-        var_dump($result);
-    } catch (AwsException $e) {
-        // output error message if fails
-        print("ERROR!");
-        var_dump($e->getMessage());
-    }
-
-
-
+try {
+    $result = $client->sendMessage($params);
+    var_dump($result);
+} catch (AwsException $e) {
+    // output error message if fails
+    error_log($e->getMessage());
+}
+ 
+ 
 ?>
